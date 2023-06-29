d183e79b4a64d3e891cb14c9fd8f65f3e70db21a
nojoven <work.cedricjoseph@gmail.com>
2023-06-29 13:19:17 +0200

    test: change credentials

--- PurBeurre/settings.py
+++ PurBeurre/settings.py
@@ -150,14 +150,22 @@ STATIC_URL = "/static/"
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
-print(config['EMAIL_HOST_PASSWORD'])
+
 # Email
 EMAIL_BACKEND = config['EMAIL_BACKEND']
-EMAIL_HOST = config['EMAIL_HOST']
-EMAIL_PORT =  config['EMAIL_PORT']
-EMAIL_HOST_USER = config['EMAIL_HOST_USER']
-EMAIL_HOST_PASSWORD = config['EMAIL_HOST_PASSWORD']
+EMAIL_HOST = "mail53.lwspanel.com"
 EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "cedric@trackon-music.com"
+EMAIL_HOST_PASSWORD = ""
+# EMAIL_HOST = "smtp.mailgun.org"
+# EMAIL_PORT =  config['EMAIL_PORT']
+# EMAIL_HOST_USER = config["EMAIL_HOST_USER"]
+EMAIL_USE_SSL = config["EMAIL_USE_SSL"]
+EMAIL_USE_TLS = config["EMAIL_USE_TLS"]
+# EMAIL_HOST_PASSWORD = config["EMAIL_HOST_PASSWORD"]
+#EMAIL_FILE_PATH = BASE_DIR / "sent_emails"
+#DEFAULT_FROM_EMAIL = config['DEFAULT_FROM_EMAIL']
 
 # redirection
 LOGIN_URL = config['LOGIN_URL']
